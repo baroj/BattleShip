5198e5b4f640aacfc4eb4bb25b258d98f1e32096
baroj <johnattanbarona@gmail.com>
2014-04-13 10:34:14 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,1 +1,18 @@
 //Board Class
+
+
+public class Board { 
+
+  public Board(String [][] board) {
+    for (int r =0; r<board.length; r++){
+      for(int c =0; c <board[0].length; c++){
+        board[r][c]= "~";
+      }
+    }
+  }
+
+    
+  
+  
+  
+}
