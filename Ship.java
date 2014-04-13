ea165cc2498cc297c596bf055ee52d5074d4e6f1
baroj <johnattanbarona@gmail.com>
2014-04-13 10:43:35 -0400

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -1,1 +1,28 @@
 //Ship Class
+
+
+public class Ship {
+  
+  public void createShip(String[][] board, int size)
+  {
+    for (int j= 0; j< 5; j+++)
+    {
+      if(Math.random() < 0.5){
+        int col = (int)(Math.random()*5);
+        int row = (int)(Math.random()*7);
+        for(int i =0; i<size; i++){
+          board[row][col+i]="S";
+        }
+      }
+      else{
+        int col = (int)(Math.random() *7);
+        int row = (int)(Math.random()*5);
+        for(int i =0; i<size; i++){
+          board[row][col+i]="S";
+        }
+      }
+    }
+  }
+  
+  
+}
