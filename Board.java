32946dfbf119da6259e998cf80992cc9064f7690
baroj <johnattanbarona@gmail.com>
2014-04-13 19:09:03 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,18 +1,74 @@
-//Board Class
+/**
+ * 
+ * @author Jonathan and Justin
+ */
+public class Board extends Ship {
+	public static final boolean DEBUG = false; // what is this for? When I comment the code out nothing changes. Hm?
 
+	/**
+	 * Constructs an empty game board.
+	 * @param size Size of game board.
+	 */
+	public Board(int size) {
+		boardSize = size;
+		grid = new String[boardSize][boardSize];		
+		for (int r = 0; r < boardSize; r++) {
+			for (int c = 0; c < boardSize; c++) {
+				grid[r][c] = "~";
+			}
+		}
+	}
 
-public class Board { 
+	
+	/**
+	 * Display board to console
+	 * @param board Array used to represent game board
+	 */
+	public void displayBoard() {
+		for (int r = 0; r < boardSize; r++) {
+			/*if (DEBUG == true) {
+				for (int c = 0; c < board[0].length; c++) {
+					System.out.print(" " + board[r][c]);		Do we need this? What is it doing?
+				}
+				System.out.println("");
+			} else {*/
+			
+			for (int c = 0; c < boardSize; c++) {
+				if (grid[r][c].equals("S")) {
+					System.out.print(" " + "~");
+				} else {
+					System.out.print(" " + grid[r][c]);
+				}
+			}
+			System.out.println("");
+		}
+	}
+	
 
-  public Board(String [][] board) {
-    for (int r =0; r<board.length; r++){
-      for(int c =0; c <board[0].length; c++){
-        board[r][c]= "~";
-      }
-    }
-  }
+	/**
+	 * Prints game board to console with ship locations revealed, for debugging purposes.
+	 * @param board Game board currently being used.
+	 */
+	public void displayShips() {
+		for (int r = 0; r < boardSize; r++) {
+			for (int c = 0; c < boardSize; c++) {
+				System.out.print(" " + grid[r][c]);
+			}
+			System.out.println("");
+		}
+	}
 
-    
-  
-  
-  
+	/**
+	 * Gets value of current board size.
+	 * @return The size of current board.
+	 */
+	public int getSize(){
+		return boardSize;
+	}
+	
+	
+	
+	public String[][] grid;
+	private int boardSize;	//by setting this value in the constructor - we only need to call .length() once
+							//It's more efficient to do it this way
 }
