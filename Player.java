984317f4b8042e4489f712b281101f4aafaa491d
Justin Johnson <johnsonj561@gmail.com>
2014-04-13 23:34:18 -0400

    Update Player.java
    Broke the previous userFire() method down into several smaller methods. This will hopefully simplify things when transitioning from console display to GUI display.

--- Player.java
+++ Player.java
@@ -6,16 +6,15 @@ import java.util.Scanner;
  * @author Jonathan and Justin
  */
 public class Player {
-	
-	//The way this is currently designed - torpedos is not being used. I don't think we need it either. Prob remove it.
-	
-	/*
-	 * Constructor initializes values
+		
+	/**
+	 * Construct new player object and initialize values.
 	 */
 	public Player(){
 		input = new Scanner(System.in);
 		hits = 0;
 		fireAgain = false;
+		shotsFired = 0;
 	}
 	
 	/**
@@ -27,51 +26,66 @@ public class Player {
     public void userFire(Board board){
          fireAgain = true;
          while(fireAgain){
-        	 
-        	 //Request a row from the user.
-        	 System.out.println("Select a row to fire in: ");
-        	 row = input.nextInt();	      	 
-        	 //Check for valid entry and request another entry if not valid
-        	 while(!(row > 0 && row < 9)){
-        		 System.out.println("Enter a valid row (1 -> 8)");
-        		 row = input.nextInt();						   		 										
-        	 }		
+        	 //Get User's Input Coordinates
+        	 do {    		 
+        		 getUserRow();	 
+        	 }while(!(row > 0 && row < 9));		   		 											
         	 row--;		//Subtract 1 from row value because array index starts at 0
         	 
-        	 //Request a column from the user.
-        	 System.out.println("Select a column to fire in: ");
-        	 col = input.nextInt();
-        	 //Check for valid entry and request another entry if not valid
-        	 while(!(col > 0 && col < 9)){	 
-        		 System.out.println("Enter a valid col (1 -> 8)");
-        		 col = input.nextInt();		 	
-        	 }	
-        	 col--;		//Subtract 1 from column value because array index starts at 0
-         
-        	 //If there is a ship at the coordinate - handle a hit!
-        	 if(board.grid[row][col].equals("S")){			  
-        		 hits ++;
-        		 System.out.println("~~~~~~~ HIT ~~~~~~~");
-        		 board.grid[row][col] = "!";
-        		 fireAgain = false;		//Some versions of battleship allow user to fire until they miss
-        		 						//changing this to 'true' would in turn allow user to shoot til they miss
-        	 }
+        	 do {    		 
+        		 getUserColumn();	 
+        	 }while(!(col > 0 && col < 9));		   		 											
+        	 col--;		//Subtract 1 from column value because array index starts at 0 
         	 
-        	 //Handles a shot selection that has already been previously selected
-        	 else if (board.grid[row][col].equals("M") || board.grid[row][col].equals("!")){
-        		 System.out.println("You Shot here already");
-        		 	// k is already 0 - so we shouldn't need to change it at all. this is not necessary
-        	 }
-        	 
-        	 //Handles a shot selection that is a miss.
-        	 else{
-        		 System.out.println("~~~~~~~ MISS ~~~~~~~");
-        		 board.grid[row][col] = "M";
-        		 fireAgain = false;		//since the player missed, flag is set to false and thus exits main while() loop	
-        	 } 
+        	 updateGrid(board);    //update the current grid with the user's move  	 
          }
     }
  
+    /**
+     * Requests and stores a valid row from user input.
+     */
+    public void getUserRow(){
+   	 	System.out.println("Select a row to fire in (1 -> 8) : ");
+   	 	row = input.nextInt();
+    }
+    
+    
+    /**
+     * Requests and stores a valid column from user input.
+     */
+    public void getUserColumn(){
+    	 System.out.println("Select a column to fire in (1 -> 8) : ");
+    	 col = input.nextInt();
+    }
+    
+    /**
+     * Update board with next move.
+     */
+    public void updateGrid(Board board){
+    	//If there is a ship at the coordinate - handle a hit!
+   	 	if(board.grid[row][col].equals("S")){			  
+   	 		hits++;
+   	 		shotsFired++;
+   	 		System.out.println("~~~~~~~ HIT ~~~~~~~");
+   	 		board.grid[row][col] = "!";
+   	 		fireAgain = false;		//Some versions of battleship allow user to fire until they miss
+   		 						//changing this to 'true' would in turn allow user to shoot til they miss
+   	}
+   	 
+   	 	//Handles a shot selection that has already been previously selected
+   	 	else if (board.grid[row][col].equals("M") || board.grid[row][col].equals("!")){
+   	 		System.out.println("You Shot here already");
+   	 	}
+   	 
+   	 	//Handles a shot selection that is a miss.
+   	 	else{
+   	 		shotsFired++;
+   	 		System.out.println("~~~~~~~ MISS ~~~~~~~");
+   	 		board.grid[row][col] = "M";
+   	 		fireAgain = false;		//since the player missed, flag is set to false and thus exits main while() loop	
+   	 	} 
+    }	
+    
     
     /**
      * Gets current hit count.
@@ -81,10 +95,19 @@ public class Player {
     	return hits;
     }
     
+    
+    /**
+     * 
+     * @return Total number of shots fired by player.
+     */
+    public int getShotsFired(){
+    	return shotsFired;
+    }
+    
+    private int shotsFired;
     private int hits;
 	private static Scanner input;
 	private int row;
 	private int col;
 	private boolean fireAgain;
 }
-
