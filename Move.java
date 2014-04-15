82e9f53c763a43237bb844b28e11a77fe2601cbb
Justin Johnson <johnsonj561@gmail.com>
2014-04-14 22:56:00 -0400

    Update Move.java
    Created methods isEast(),isWest(),isNorth(), and isSouth()to compare locations of various moves.
    
    These will simplify process when utilizing Advanced skill set of AI.

--- Move.java
+++ Move.java
@@ -3,44 +3,119 @@
 
 /**
  * Class for handling moves 
- * @author Pustikins
+ * @author Jonathan and Justin
  *
  */
 public class Move {
 	
-	
+	/**
+	 * Construct move and assign coordinates.
+	 * @param x
+	 * @param y
+	 */
 	public Move(int x, int y){
 		xCord = x;
 		yCord = y;
 	}
 	
-	//return xCord
+	/**
+	 * Get value of x-coordinate.
+	 * @return Value of x-coordinate.
+	 */
 	public int getX(){
 		return xCord;
 	}
 	
-	//return yCord
+	/**
+	 * Get value of y-coordinate.
+	 * @return Value of y-coordinate.
+	 */
 	public int getY(){
 		return yCord;
 	}
 	
-	//set value of xCord
+	/**
+	 * Set value of x-coordinate.
+	 * @param x New value for x-coordinate.
+	 */
 	public void setX(int x){
 		xCord = x;
 	}
 	
-	//set value of yCord
+	/**
+	 * Set value of y-coordinate.
+	 * @param y New value for y-coordinate.
+	 */
 	public void setY(int y){
 		yCord = y;
 	}
 	
-	
-	
-	//Create a string out of Coordinates
-	//Format is: "xCord, yCord"
+	/**
+	 * Convert x and y coordinates to a string with structure : "x, y"
+	 */
 	public String toString(){
 		return xCord + ", " + yCord;
 	}
+	
+	/**
+	 * Check to see if this move's coordinates is directly above Move 'other'.
+	 * @param other Move being compared with.
+	 * @return True if this move's coordinates is directly above Move 'other'.
+	 */
+	public Boolean isNorth(Move other){
+		if((this.getX() == other.getX()) && (this.getY() == (other.getY()-1))){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+		
+	/**
+	 * Check to see if this move's coordinates is directly below Move 'other'.
+	 * @param other Move being compared with.
+	 * @return True if this move's coordinates is directly below Move 'other'.
+	 */
+	public Boolean isSouth(Move other){
+		if((this.getX() == other.getX()) && (this.getY() == (other.getY()+1))){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/**
+	 * Check to see if this move's coordinates is directly to right of Move 'other'.
+	 * @param other Move being compared with.
+	 * @return True if this move's coordinates is directly to right of Move 'other'.
+	 */
+	public Boolean isEast(Move other){
+		if((this.getY() == other.getY()) && (this.getX() == (other.getX()+1))){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	/**
+	 * Check to see if this move's coordinates is directly to left of Move 'other'.
+	 * @param other Move being compared with.
+	 * @return True if this move's coordinates is directly to left of Move 'other'.
+	 */
+	public Boolean isWest(Move other){
+		if((this.getY() == other.getY()) && (this.getX() == (other.getX()-1))){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	
+	
+	
 	
 	
 	private int xCord;
