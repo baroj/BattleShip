fc83282afff329e6bcc658dc7b00c473fd7bdd2d
Justin Johnson <johnsonj561@gmail.com>
2014-04-16 23:32:19 -0400

    Update Move.java
    Methods added to simplify move selection for Artificial Intelligence:
    isValidMove() returns true if coordinate is not already fired upon and falls within grid range.
    
    Currently - x and y coordinates are reversed somewhere, resulting in logic errors at run time. Will locate error and commit changes as needed.

--- Move.java
+++ Move.java
@@ -7,7 +7,7 @@
  *
  */
 public class Move {
-	
+
 	/**
 	 * Construct move and assign coordinates.
 	 * @param x
@@ -17,7 +17,7 @@ public class Move {
 		xCord = x;
 		yCord = y;
 	}
-	
+
 	/**
 	 * Get value of x-coordinate.
 	 * @return Value of x-coordinate.
@@ -25,7 +25,7 @@ public class Move {
 	public int getX(){
 		return xCord;
 	}
-	
+
 	/**
 	 * Get value of y-coordinate.
 	 * @return Value of y-coordinate.
@@ -33,7 +33,7 @@ public class Move {
 	public int getY(){
 		return yCord;
 	}
-	
+
 	/**
 	 * Set value of x-coordinate.
 	 * @param x New value for x-coordinate.
@@ -41,7 +41,7 @@ public class Move {
 	public void setX(int x){
 		xCord = x;
 	}
-	
+
 	/**
 	 * Set value of y-coordinate.
 	 * @param y New value for y-coordinate.
@@ -49,14 +49,14 @@ public class Move {
 	public void setY(int y){
 		yCord = y;
 	}
-	
+
 	/**
 	 * Convert x and y coordinates to a string with structure : "x, y"
 	 */
 	public String toString(){
 		return xCord + ", " + yCord;
 	}
-	
+
 	/**
 	 * Check to see if this move's coordinates is directly above Move 'other'.
 	 * @param other Move being compared with.
@@ -70,7 +70,7 @@ public class Move {
 			return false;
 		}
 	}
-		
+
 	/**
 	 * Check to see if this move's coordinates is directly below Move 'other'.
 	 * @param other Move being compared with.
@@ -84,7 +84,7 @@ public class Move {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Check to see if this move's coordinates is directly to right of Move 'other'.
 	 * @param other Move being compared with.
@@ -98,7 +98,7 @@ public class Move {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Check to see if this move's coordinates is directly to left of Move 'other'.
 	 * @param other Move being compared with.
@@ -112,12 +112,67 @@ public class Move {
 			return false;
 		}
 	}
+
+	/**
+	 * Check to see if this coordinate is a ship hit.
+	 * @param move The move coordinates that are being checked.
+	 * @param board The game board being checked for a ship hit.
+	 * @return True if move coordinate is a hit.
+	 */
+	public Boolean isHit(Board board){
+		if(board.grid[this.getY()][this.getX()] == "!"){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 	
+	/**
+	 * Check to see if this coordinate is a a miss shot.
+	 * @param move The move coordinates that are being checked.
+	 * @param board The game board being checked for a missed shot..
+	 * @return True if move coordinate is a missed shot.
+	 */
+	public Boolean isMiss(Board board){
+		if(board.grid[this.getY()][this.getX()] == "M"){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	/**
+	 * Check to see if this Move is a valid move.
+	 * @param move Coordinates being checked for validity.
+	 * @param board Board being checked for validity.
+	 * @return True if current coordinate is not already fired upon and is valid coordinate on grid.
+	 */
+	public Boolean isValidMove(Board board){
+		int col = this.getX();
+		int row = this.getY();
+		//if move is outside of the grid size - is not valid
+		if((col < 1) || (col > Board.getSize()) || (row < 1) || (row > Board.getSize())){	
+			System.out.println("Invalid Move Detected");
+			return false;															
+		}
+		//else if coordinate contains anything other than "~" - is has already been shot at, thus not valid
+		else if(board.grid[row][col] != "~"){
+			System.out.println("Invalid Move Detected");
+			return false;
+		}
+		//else coordinate is a valid move
+		else{
+			System.out.println("Valid Move Detected");
+			return true;
+		}
+		
+	}
 	
+
 	
-	
-	
-	
+
 	private int xCord;
 	private int yCord;
 }
