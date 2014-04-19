263f8ea32d404d7f441f4351339d26493ac2e94c
Justin Johnson <johnsonj561@gmail.com>
2014-04-19 01:34:26 -0400

    Update BattleShipGame.java

--- BattleShipGame.java
+++ BattleShipGame.java
@@ -4,7 +4,7 @@
  *
  */
 public class BattleShipGame {
-	
+
 	/**
 	 * Construct game boards and players accordingly.
 	 * @param skill The skill level to be applied to Computer Player AI
@@ -19,22 +19,22 @@ public class BattleShipGame {
 		System.out.println("Placing computer ships on computer's board");
 		compBoard.createShip(compBoard, 4);
 		compBoard.displayShips();
-		user = new Player();				//Construct user player object
+		user = new UserPlayer();				//Construct user player object
         comp = new AIPlayer(skill);			//Construct computer player object
 	}
-	
-	
+
+
 	/**
 	 * Take turns firing until there is a winner.
 	 */
 	public void playGame(){        										
-		while ((user.getHits() < userBoard.hitsRequired()) &&	//while there is no winner!
-        		(comp.getHits() < compBoard.hitsRequired())){	//PLAY BATTLESHIP!
+		while ((user.getHitCount() < userBoard.hitsRequired()) &&	//while there is no winner!
+        		(comp.getHitCount() < compBoard.hitsRequired())){	//PLAY BATTLESHIP!
         	if(takeTurns){
         		System.out.println("USER'S TURN\nUSER'S GAMEBOARD:");		//display board
             	userBoard.displayBoard();
             	System.out.println();
-            	user.userFire(userBoard);				//user selects shot
+            	user.playerFire(userBoard);				//user selects shot
             	System.out.println("USER HAS FIRED!");
             	userBoard.displayBoard();
             	System.out.println("\n\n\n");
@@ -43,7 +43,7 @@ public class BattleShipGame {
             	System.out.println("COMPUTER'S TURN\nCOMPUTER'S GAMEBOARD:");	//display board
             	compBoard.displayBoard();				
             	System.out.println();;				
-            	comp.compFire(compBoard);				//computer selects shot
+            	comp.playerFire(compBoard);				//computer selects shot
             	System.out.println("COMPUTER HAS FIRED!");
             	compBoard.displayBoard();
             	System.out.println("\n\n\n");
@@ -51,30 +51,31 @@ public class BattleShipGame {
             //Once a player has gone, switch whose turn it is
             takeTurns = !takeTurns;
         }
-		
-		
+
+
 	}
-	
-	
+
+
 	/**
 	 * Display end of game results.
 	 */
 	public String gameOver(){
-		if(user.getHits() < comp.getHits()){
-			return "Sorry, but you've lost!.";
+		if(user.getHitCount() < comp.getHitCount()){
+			return "Sorry, but you've lost!.\nComputer fired " + comp.getShotsFired();
 		}
 		else{
-			return "Congratulations, You've Won!!";
+			return "Congratulations, You've Won!!\nYou fired " + user.getShotsFired();
+			
 		}
 	}
-	
-	
-	
-	
+
+
+
+
 	public Board compBoard;
 	public Board userBoard;
 	private Boolean takeTurns;
-	private Player user;
+	private UserPlayer user;
 	private AIPlayer comp;
 	private final int boardSize = 8;
 }
