2d88b77b7901752a7078656e35fb185849e8e03f
Justin Johnson <johnsonj561@gmail.com>
2014-04-19 00:11:51 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -13,31 +13,37 @@ public class Player {
 	public Player(){
 		input = new Scanner(System.in);
 		hits = 0;
-		fireAgain = false;
 		shotsFired = 0;
+		fireAgain = false;
+		nextMove = new Move(-1, -1);
+		
 	}
 	
 	/**
-	 * Handles users move
-	 * @param board Game board that the user is firing on.
-	 * @param hits The total number of hits already accumulated.
-	 * @return Integer value that represents hits.
+	 * Get next move from user, check validity of next move, and update grid.
+	 * @param board Game board that the user is firing at.
 	 */
     public void userFire(Board board){
          fireAgain = true;
          while(fireAgain){
+        	 
         	 //Get User's Input Coordinates
         	 do {    		 
         		 getUserRow();	 
         	 }while(!(row > 0 && row < (Board.getSize()+1)));		   		 											
         	 row--;		//Subtract 1 from row value because array index starts at 0
-        	 
+        	 nextMove.setRow(row);
         	 do {    		 
-        		 getUserColumn();	 
+        		 getUserColumn();
         	 }while(!(col > 0 && col < (Board.getSize()+1)));		   		 											
         	 col--;		//Subtract 1 from column value because array index starts at 0 
+        	 nextMove.setCol(col);
         	 
-        	 updateGrid(board);    //update the current grid with the user's move  	 
+        	 //if user's next move is a valid move - update the grid and exit loop
+        	 if(nextMove.isValidMove(board)){
+        		 fireAgain = false;
+        		 placeShot(board, nextMove);
+        	 } 	 
          }
     }
  
@@ -46,7 +52,7 @@ public class Player {
      */
     public void getUserRow(){
    	 	System.out.println("Select a row to fire in (1 -> " + Board.getSize() + ") : ");
-   	 	row = input.nextInt();
+   	 	row = input.nextInt(); 	 	
     }
     
     
@@ -57,34 +63,21 @@ public class Player {
     	 System.out.println("Select a column to fire in ((1 -> " + Board.getSize() + ") : ");
     	 col = input.nextInt();
     }
-    
+   
+
     /**
-     * Update board with next move.
+     * Handles shot and increments player's hit counter and shotsFired counter accordingly.
+     * @param board Game board shot is being placed on.
+     * @param nextMove Coordinates of move being placed on grid.
      */
-    public void updateGrid(Board board){
-    	//If there is a ship at the coordinate - handle a hit!
-   	 	if(board.grid[row][col].equals("S")){			  
-   	 		hits++;
-   	 		shotsFired++;
-   	 		System.out.println("~~~~~~~ HIT ~~~~~~~");
-   	 		board.grid[row][col] = "!";
-   	 		fireAgain = false;		//Some versions of battleship allow user to fire until they miss
-   		 						//changing this to 'true' would in turn allow user to shoot til they miss
-   	}
-   	 
-   	 	//Handles a shot selection that has already been previously selected
-   	 	else if (board.grid[row][col].equals("M") || board.grid[row][col].equals("!")){
-   	 		System.out.println("You Shot here already");
-   	 	}
-   	 
-   	 	//Handles a shot selection that is a miss.
-   	 	else{
-   	 		shotsFired++;
-   	 		System.out.println("~~~~~~~ MISS ~~~~~~~");
-   	 		board.grid[row][col] = "M";
-   	 		fireAgain = false;		//since the player missed, flag is set to false and thus exits main while() loop	
-   	 	} 
-    }	
+    public void placeShot(Board board, Move nextMove){
+		 board.updateGrid(nextMove);
+		 if(nextMove.isHit(board)){		//if grid update resulted in a hit - increment hits count
+			 hits++;					
+		 }
+		 shotsFired++;
+
+    }
     
     
     /**
@@ -104,11 +97,13 @@ public class Player {
     	return shotsFired;
     }
     
+    
+	private static Scanner input;
+    private Move nextMove;
+	private boolean fireAgain;
     private int shotsFired;
     private int hits;
-	private static Scanner input;
 	private int row;
 	private int col;
-	private boolean fireAgain;
 }
 
