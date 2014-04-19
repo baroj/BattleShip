4aa6bf50e1799ca0667e17e058d9b695db1c0cf9
Justin Johnson <johnsonj561@gmail.com>
2014-04-19 01:30:29 -0400

    Update Player.java
    Designed an abstract Player class that contains the common methods and fields of both AIPlayer and UserPlayer. This simplifies design for future changes/implementations.

--- Player.java
+++ Player.java
@@ -1,82 +1,39 @@
-
-import java.util.Scanner;
-
 /**
- *
+ * Abstract Player class containing common member methods and fields.
  * @author Jonathan and Justin
  */
-public class Player {
-		
-	/**
-	 * Construct new player object and initialize values.
-	 */
+public abstract class Player{
+	
 	public Player(){
-		input = new Scanner(System.in);
-		hits = 0;
+		System.out.println("In Abstract Player() Constructor");
 		shotsFired = 0;
+		hits = 0;
 		fireAgain = false;
+		prevMove = new Move(-1, -1);	//moves initialized to invalid moves to prevent un-desired grid updates
 		nextMove = new Move(-1, -1);
-		
 	}
 	
+	
 	/**
-	 * Get next move from user, check validity of next move, and update grid.
+	 * Get next move from player, check validity of next move, and handle move appropriately.
 	 * @param board Game board that the user is firing at.
 	 */
-    public void userFire(Board board){
-         fireAgain = true;
-         while(fireAgain){
-        	 
-        	 //Get User's Input Coordinates
-        	 do {    		 
-        		 getUserRow();	 
-        	 }while(!(row > 0 && row < (Board.getSize()+1)));		   		 											
-        	 row--;		//Subtract 1 from row value because array index starts at 0
-        	 nextMove.setRow(row);
-        	 do {    		 
-        		 getUserColumn();
-        	 }while(!(col > 0 && col < (Board.getSize()+1)));		   		 											
-        	 col--;		//Subtract 1 from column value because array index starts at 0 
-        	 nextMove.setCol(col);
-        	 
-        	 //if user's next move is a valid move - update the grid and exit loop
-        	 if(nextMove.isValidMove(board)){
-        		 fireAgain = false;
-        		 placeShot(board, nextMove);
-        	 } 	 
-         }
-    }
- 
-    /**
-     * Requests and stores a valid row from user input.
-     */
-    public void getUserRow(){
-   	 	System.out.println("Select a row to fire in (1 -> " + Board.getSize() + ") : ");
-   	 	row = input.nextInt(); 	 	
-    }
+    public abstract void playerFire(Board board);
     
     
     /**
-     * Requests and stores a valid column from user input.
-     */
-    public void getUserColumn(){
-    	 System.out.println("Select a column to fire in ((1 -> " + Board.getSize() + ") : ");
-    	 col = input.nextInt();
-    }
-   
-
-    /**
-     * Handles shot and increments player's hit counter and shotsFired counter accordingly.
+     * Sends update to board's grid and increments player's hit counter and shotsFired counter accordingly.
      * @param board Game board shot is being placed on.
      * @param nextMove Coordinates of move being placed on grid.
      */
     public void placeShot(Board board, Move nextMove){
 		 board.updateGrid(nextMove);
-		 if(nextMove.isHit(board)){		//if grid update resulted in a hit - increment hits count
+		 prevMove = nextMove;
+		 if(prevMove.isHit(board)){		//if grid update resulted in a hit - increment hits count
 			 hits++;					
 		 }
 		 shotsFired++;
-
+		 fireAgain = false;
     }
     
     
@@ -98,12 +55,11 @@ public class Player {
     }
     
     
-	private static Scanner input;
-    private Move nextMove;
-	private boolean fireAgain;
+    public Move prevMove;
+    public Move nextMove;
+	public boolean fireAgain;
     private int shotsFired;
-    private int hits;
-	private int row;
-	private int col;
+    public int hits;
+
 }
 
