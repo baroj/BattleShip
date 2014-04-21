3dd05afe6c95815c2708bac9682545757a876375
Justin Johnson <johnsonj561@gmail.com>
2014-04-20 21:43:23 -0400

    Update AIPlayer.java
    onStreak(board) checks to see if this player has any recent hits, and returns an integer value based off of the results.
    Returns:
    1 = prevMove.isHit(board) && !prevMove2.isHit(board)
    2 = prevMove.isHit(board) && prevMove2.isHit(board)
    3 = prevMove2.isHit(board)
    0 = No previous hits to work with.
    
    This integer is then used in playerFire() switch statement to help determine next move selection.

--- AIPlayer.java
+++ AIPlayer.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+
 
 /**
  * Constructs the computer user and handles computer moves.
@@ -12,7 +12,6 @@ public class AIPlayer extends Player{
 	 * @param s The skill level being applied to computer player.
 	 */
 	public AIPlayer(int s){
-		generator = new Random();
 		skillLevel = s;
 		System.out.println("Computer player constructed.");
 		System.out.print("Skill level chosen: ");
@@ -33,20 +32,21 @@ public class AIPlayer extends Player{
 		
 		//SKILL LEVEL BEGINNER - shot selections are random
 		if(skillLevel==0){	
-			nextMove = getRandomMove();
+			nextMove = BSStrategy.getRandomMove();
 			while(!nextMove.isValidMove(board)){
-				nextMove = getRandomMove();
+				nextMove = BSStrategy.getRandomMove();
 			}
 			placeShot(board, nextMove);
 		}
 		
 		//SKILL LEVEL ADVANCED - computer utilizes battleship strategies
-		else{			
+		else{
+			
 			if(getShotsFired() == 0){	//If first shot of game, select shot at random
 				System.out.println("First shot of game.");
-				nextMove = getRandomMove();
+				nextMove = BSStrategy.getRandomMove();
 				while(!nextMove.isValidMove(board)){
-					nextMove = getRandomMove();
+					nextMove = BSStrategy.getRandomMove();
 				}
 				placeShot(board, nextMove);
 			}
@@ -55,106 +55,134 @@ public class AIPlayer extends Player{
 			
 			else{	//If not first shot of game - utilize battleship strategy
 				
-				//if previous move is a hit - get a local move
-				if(prevMove.isHit(board)){	
-					System.out.println("prevMove.isHit: " + prevMove.toString());
-					nextMove = getLocalMove(prevMove, board);
-					//if local move is a valid move - place it on the grid
-					if(nextMove.isValidMove(board)){
-						System.out.println("nextMove.isValid: " + nextMove.toString());
-						System.out.println("Placing next local move: " + nextMove.toString());
-						placeShot(board, nextMove);
-						
-					}
-					//else - no local moves are available - resort to getting random moves
-					else{
-						System.out.println("Local move not valid, return to random move.");
+				//check previous hits for potential hit streak, choose next move based off previous hits
+				switch(onStreak(board)){
+					
+					case 0:	//prevMove and prevMove2 are not hits
+						System.out.println("Case 0: No previous moves to work with.");
 						while(!nextMove.isValidMove(board)){	//while move is not valid - get another move
-							nextMove = getRandomMove();
+							nextMove = BSStrategy.getRandomMove();
 						}
 						System.out.println("Placing random shot: " + nextMove.toString());
-						placeShot(board, nextMove);				
-					}
-					
-				}
+						placeShot(board, nextMove);																				
+						break;
+					case 1: //prevMove is hit, prevMove2 is not a hit
+						System.out.println("prevMove.isHit: " + prevMove.toString());
+						nextMove = BSStrategy.getLocalMove(prevMove, board);
+						//if local move is a valid move - place it on the grid
+						if(nextMove.isValidMove(board)){
+							System.out.println("nextMove.isValid: " + nextMove.toString());
+							System.out.println("Placing next local move: " + nextMove.toString());
+							placeShot(board, nextMove);
+						}
+						else{
+							System.out.println("Local move not valid, return to random move.");
+							while(!nextMove.isValidMove(board)){	//while move is not valid - get another move
+								nextMove = BSStrategy.getRandomMove();
+							}
+							System.out.println("Placing random shot: " + nextMove.toString());
+							placeShot(board, nextMove);	
+						}
+						break;
+						
+						
+					case 2:	//prevMove and prevMove2 are both hits
+						System.out.println("prevMove.isHit: " + prevMove.toString());
+						System.out.println(" && prevMove2.isHit: " + prevMove2.toString());
+						nextMove = BSStrategy.continueStreak(prevMove, board, BSStrategy.getStreakDirection(prevMove, prevMove2));
+						if(nextMove.isValidMove(board)){
+							System.out.println("nextMove.isValid: " + nextMove.toString());
+							System.out.println("Placing next local move: " + nextMove.toString());
+							placeShot(board, nextMove);
+						}
+						else{
+							nextMove = BSStrategy.continueStreak(prevMove2, board, BSStrategy.getStreakDirection(prevMove2, prevMove));
+							if(nextMove.isValidMove(board)){
+								System.out.println("nextMove.isValid: " + nextMove.toString());
+								System.out.println("Placing next local move: " + nextMove.toString());
+								placeShot(board, nextMove);
+							}
+							else{
+								while(!nextMove.isValidMove(board)){	//while move is not valid - get another move
+									nextMove = BSStrategy.getRandomMove();
+								}
+								System.out.println("Placing random shot: " + nextMove.toString());
+								placeShot(board, nextMove);	
+							}
+							
+						}
+						break;
+						
+					case 3:	//prevMove2 is hit, prevMove1 is not a hit	
+						System.out.println("prevMove2.isHit: " + prevMove2.toString());
+						nextMove = BSStrategy.getLocalMove(prevMove2, board);
+						//if local move is a valid move - place it on the grid
+						if(nextMove.isValidMove(board)){
+							System.out.println("nextMove.isValid: " + nextMove.toString());
+							System.out.println("Placing next local move: " + nextMove.toString());
+							placeShot(board, nextMove);
+						}
+						else{
+							System.out.println("Local move not valid, return to random move.");
+							while(!nextMove.isValidMove(board)){	//while move is not valid - get another move
+								nextMove = BSStrategy.getRandomMove();
+							}
+							System.out.println("Placing random shot: " + nextMove.toString());
+							placeShot(board, nextMove);	
+						}
 				
-				//else previous move is not a hit - so resort to random move and place on board
-				else{
-					System.out.println("prevMove is not a hit - get random move.");
-					nextMove = getRandomMove();
-					while(!nextMove.isValidMove(board)){
-						nextMove = getRandomMove();
-					}
-					placeShot(board, nextMove);
+						break;
+					default:
+						System.out.println("Error selecting next move.");
+				
 				}
 			}
-		}	
+		}
 	}
 
+
+    
+    
+   
+    
     /**
-     * Generates a random Move.
-     * @return
+     * 
+     * @param board Game board being played on.
+     * @return Integer value used to represent previous move hits.
+     * 			1 = prevMove.isHit(board) && !prevMove2.isHit(board)
+     * 			2 = prevMove.isHit(board) && prevMove2.isHit(board)
+     * 			3 = prevMove2.isHit(board)
+     * 			0 = No previous hits to work with.
      */
-    public Move getRandomMove(){
-    	Move randMove = new Move(generator.nextInt(Board.getSize()), 
-    			generator.nextInt(Board.getSize()));
-    	return randMove;
-    }
-    
-    
-    /**
-	 * Select a move that neighbors current move, return invalid Move if none are available.
-	 * @param move Coordinates being evaluated.
-	 * @return Returns a move that is directly North, South, East, or West of current Move.
-	 * @return Move(-1, -1) if there are no moves available North, South, East or West of current Move.
-	 */
-    public Move getLocalMove(Move move, Board board){
-    	int row = move.getRow();
-    	int col = move.getCol();
-    	Move localMove = new Move(row, col-1);	
-    	System.out.println("Trying localMove " + localMove.toString());
-    	if(localMove.isValidMove(board)){			//if West coordinate is valid - return
-    		System.out.println("returning local W: " + localMove.toString());
-    		return localMove;
+    public int onStreak(Board board){
+    	if(prevMove.isHit(board) && !prevMove2.isHit(board)){
+    		System.out.println("prevMove.isHit(board) && !prevMove2.isHit(board)");
+			return 1;
+    	}
+    	else if(prevMove.isHit(board) && prevMove2.isHit(board)){
+    		System.out.println("prevMove.isHit(board) && prevMove2.isHit(board)");
+    		return 2;
+    	}
+    	else if(prevMove2.isHit(board)){	
+    		System.out.println("prevMove2.isHit(board) && !prevMove.isHit(board)");
+    		return 3;
     	}
     	else{
-    		localMove = new Move(row, col +1);
-        	System.out.println("Trying localMove " + localMove.toString());
-    		if(localMove.isValidMove(board)){		//else if East coordinate is valid - return
-    			System.out.println("Returning local E: " + localMove.toString());
-    			return localMove;
-    		}
-    		else{
-    			localMove = new Move (row-1, col);
-    	    	System.out.println("Trying localMove " + localMove.toString());
-    			if(localMove.isValidMove(board)){	//else if South coordinate is valid - return
-    				System.out.println("Returning local S: " + localMove.toString());
-    				return localMove;
-    			}
-    			else{
-    				localMove = new Move(row+1, col);
-    		    	System.out.println("Trying localMove " + localMove.toString());
-    				if(localMove.isValidMove(board)){//else if North coordinate is valid - return
-    					System.out.println("Returning local N: " + localMove.toString());
-    					return localMove;
-    				}
-    				else{
-    					localMove = new Move(-1, -1);//if no localMoves are valid - return an invalid move
-    					System.out.println("Returning Invalid Local Move: " + localMove.toString());
-    					return localMove;			//if invalid move is returned - there are no valid moves
-    				}								//available
-    			}
-    		}
-    	}  	
+    		System.out.println("No previous hits to work with.");
+    		return 0;
+    	}
     }
+    
+    
     
     
     
    // private int localMoveCounter;	--> need way to check for another localMove if the first localMove is a miss
    //we want the computer to exhaust all 4 corners of the last hit before moving onto random shot selection
-	private Random generator;
 	private int skillLevel;
 
+	//private Move prevMove3;		can be used to increase intelligence.
+	//private Move prevMove4;
 
 
 }
